62febaf37b764a426bc8626615f2654610497dd0
Revathy Saravanya Iniavan <32856109+revafairfield@users.noreply.github.com>
2018-05-24 11:12:12 -0400

    Impute by Deleting Rows
    Impute by Deleting Rows

---
+++ RProgramming/Linear Regression/Graduate Rate.r
@@ -0,0 +1,28 @@
+library(readxl)
+New_CT_Grad <- read_excel("Desktop/capstone/Capstone/New_CT_Grad.xls", sheet="Original",
+                     col_types = c("numeric", "numeric", "numeric", 
+                                   "numeric", "numeric", "numeric", 
+                                   "numeric", "numeric", "numeric", 
+                                   "numeric", "numeric", "numeric", 
+                                   "numeric", "numeric", "numeric", 
+                                   "numeric", "numeric", "numeric", 
+                                   "numeric", "numeric", "numeric", 
+                                   "numeric", "numeric", "numeric", 
+                                   "numeric", "numeric", "numeric", 
+                                   "numeric", "numeric", "numeric", 
+                                   "numeric", "numeric", "numeric", 
+                                   "numeric", "numeric", "numeric", 
+                                   "numeric", "numeric", "numeric", 
+                                   "numeric", "numeric", "numeric", 
+                                   "numeric", "numeric", "numeric", 
+                                   "numeric", "numeric", "numeric", 
+                                   "numeric", "numeric", "numeric", 
+                                   "numeric", "numeric", "numeric", 
+                                   "numeric", "numeric", "numeric", 
+                                   "numeric", "numeric", "numeric", 
+                                   "numeric", "numeric"))
+
+res <- lm(GraduRate~., data = New_CT_Grad)
+summary(res)
+
+
